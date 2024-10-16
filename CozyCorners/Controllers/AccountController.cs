9d204945e5350c8d8cb77e19da50f889aac601dc
Arwa Alaa <arwaalaa24682468@gmail.com>
2024-10-16 20:35:03 +0300

    save

--- CozyCorners/Controllers/AccountController.cs
+++ CozyCorners/Controllers/AccountController.cs
@@ -15,7 +15,7 @@ namespace CozyCorners.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
-      //
+      
         public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
